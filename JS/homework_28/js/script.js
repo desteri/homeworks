23b8d27945e892e9c5bd6331bf716c0ec08d964b
desteri <kyubei.dark@gmail.com>
2021-04-05 22:19:28 +0600

    added 28 homework

---
+++ JS/homework_28/js/script.js
@@ -0,0 +1,7 @@
+"use strict";
+
+let image = document.getElementsByClassName("image");
+let title = document.getElementsByClassName("title");
+let price = document.getElementsByClassName("price");
+
+console.log(image, title, price);
