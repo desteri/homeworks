5091a92229df9c9bfe7a6b228df6d389c9aa1ec4
desteri <kyubei.dark@gmail.com>
2021-04-09 20:48:46 +0600

    Added 29 homework

---
+++ JS/homework_29/js/script.js
@@ -0,0 +1,10 @@
+"use strict";
+
+// Задание 1
+let prdct = document.getElementsByClassName("list__item");
+
+for(let list of prdct) {
+    console.log(list.innerText);
+}
+
+// Задание 2, 3, 4, 5 решу позже, когда дойдем до событий
