3f74eb79c5867a2ca90301d797fbc335d1b62456
desteri <kyubei.dark@gmail.com>
2021-05-12 15:39:07 +0600

    added 42 homework

---
+++ JS/homework_42/js/add_to_card.js
@@ -0,0 +1,125 @@
+function addProduct() {
+    
+    "use strict";
+    let className = document.getElementsByClassName("add__card");
+
+    let myFunction = function(event) {
+        
+        event.preventDefault();
+        
+        let title = this.parentNode.parentNode.parentNode.querySelectorAll(".item__title a");
+        let price = this.parentNode.parentNode.parentNode.querySelectorAll(".price .price__sum");
+        let image = this.parentNode.parentNode.parentNode.querySelectorAll(".item__img img");
+        let delPrice = this.parentNode.parentNode.parentNode.querySelectorAll(".price del");
+        
+        let createLi = document.createElement('li');
+    
+        createLi.innerHTML = '<div class="bag__item">' +
+                                '<div class="bag__img">' +
+                                    '<a href="#">' +
+                                        '<img src="' + image[0].src + '" alt="">' +
+                                    '</a>' +
+                                '</div>' +
+                                '<div class="bag__info">' +
+                                    '<p class="info__title"><a href="#">' + title[0].innerText + '</a></p>' +
+                                    '<div class="info__price">' +
+                                        '<span class="currency">$</span>' + 
+                                        '<span class="price__sum">' + price[0].innerText + '</span>' +
+                                        '<del>' + delPrice[0].innerText + '</del>' +
+                                    '</div>' +
+                                    '<div class="trash">' +
+                                        '<a href="#"><i class="far fa-trash-alt"></i></a>' +
+                                    '</div>' +
+                                '</div>' +
+                            '</div>';
+    
+        let blockCard = document.querySelector('.bag ul');
+        blockCard.appendChild(createLi);
+    
+        alert("Товары успешно добавлены в корзину!");
+        
+        delProduct();
+        totalCash();
+        updCount();
+    
+    };
+    
+    for (var i = 0; i < className.length; i++) {
+        
+        className[i].addEventListener('click', myFunction, false);
+    
+    }
+
+}
+
+function delProduct() {
+    
+    "use strict";
+    let trash = document.querySelectorAll('.trash');
+
+    let delFunction = function(event) {
+
+        event.preventDefault();
+
+        this.parentNode.parentNode.remove();
+
+        totalCash();
+        updCount();
+
+    };
+
+    for (var i = 0; i < trash.length; i++) {
+
+        trash[i].addEventListener('click', delFunction, false);
+
+    }
+
+}
+
+function totalCash() {
+
+    "use strict";
+    let priceSum = document.querySelectorAll('.info__price .price__sum');
+    let sum = 0;
+
+    for (var i = 0; i < priceSum.length; i++) {
+        
+        sum += (+priceSum[i].textContent);
+        
+    }    
+    
+    document.getElementsByClassName('number__price')[0].innerHTML = `$${sum}`;
+    document.querySelectorAll('.shopcard__total .total__sum')[0].innerHTML = `$${sum}`;
+
+}
+
+function updCount() {
+    
+    let cardCount = document.getElementsByClassName('bag__item').length;
+    
+    document.getElementsByClassName('number__count')[0].innerHTML = cardCount;
+}
+
+addProduct();
+
+// ДЗ №42
+function menu() {
+
+    "use strict";
+    let btn = document.getElementsByClassName('mb');
+
+    let delBtn = (event) => {
+        
+        event.target.remove();
+
+    };
+
+    for(var i = 0; i < btn.length; i++) {
+
+        btn[i].addEventListener('click', delBtn);
+
+    }
+
+}
+
+menu();
